93f02860704b7a18855adf194d5dc1db224de66e
BoYs <75466116+BoYs-J@users.noreply.github.com>
2022-06-17 13:59:20 +0800

    提交

--- keyboard/FX980/FX01S/config.h
+++ keyboard/FX980/FX01S/config.h
@@ -93,9 +93,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_MATRIX_STARTUP_SAT 255 // Sets the default saturation value, if none has been set
 #define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
 #define RGB_MATRIX_CENTER { 112, 36 }
-#define RGB_MATRIX_INDICATORS_CAPS 54
-#define RGB_MATRIX_INDICATORS_NUM 32
-#define RGB_MATRIX_INDICATORS_HOST 0
+//#define RGB_MATRIX_INDICATORS_CAPS 54
+//#define RGB_MATRIX_INDICATORS_NUM 32
+//#define RGB_MATRIX_INDICATORS_HOST 0
 
 // 3灯指示配置引脚
 // #define LED_STATUS_BLE 19
