a6b010cd9de2753d8c60304808c0354a4599768f
BoYs <75466116+BoYs-J@users.noreply.github.com>
2022-06-19 19:59:42 +0800

    提交

--- application/main/src/driver/ssd1306/ssd1306_oled.c
+++ application/main/src/driver/ssd1306/ssd1306_oled.c
@@ -327,8 +327,8 @@ static void ssd1306_event_handler(enum user_event event, void* arg)
         else if (param == PASSKEY_STATE_SEND) {
             // 清空配对码的显示
             ssd1306_clr();
-            ssd1306_show_all();
         }
+        ssd1306_event_handler(enum user_event event, void* arg);
         status_mark_dirty();
         break;
     case USER_EVT_BLE_STATE_CHANGE: // 蓝牙状态
