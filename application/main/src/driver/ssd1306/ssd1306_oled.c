8b1133d68e3ef5ce2dfcf3ea42602f556c112ac2
BoYs <75466116+BoYs-J@users.noreply.github.com>
2022-06-19 19:27:10 +0800

    提交

--- application/main/src/driver/ssd1306/ssd1306_oled.c
+++ application/main/src/driver/ssd1306/ssd1306_oled.c
@@ -328,11 +328,13 @@ static void ssd1306_event_handler(enum user_event event, void* arg)
             // 清空配对码的显示
             ssd1306_clr();
         }
+        switch (param) {
+        case KBD_STATE_INITED: // 显示Buff
         update_status_bar();
         ssd1306_show_all();
+        break;}
         status_mark_dirty();
         break;
-
     case USER_EVT_BLE_STATE_CHANGE: // 蓝牙状态
         ble_conn = (param == BLE_STATE_CONNECTED);
         status_mark_dirty();
