4e7ee35501be999d0ae8d25c1d437a3eae2fdce1
BoYs <75466116+BoYs-J@users.noreply.github.com>
2022-06-19 19:37:23 +0800

    提交

--- application/main/src/driver/ssd1306/ssd1306_oled.c
+++ application/main/src/driver/ssd1306/ssd1306_oled.c
@@ -318,16 +318,16 @@ static void ssd1306_event_handler(enum user_event event, void* arg)
         status_mark_dirty();
         break;
     case USER_EVT_BLE_PASSKEY_STATE: // 配对码状态
+        update_status_bar();
+        ssd1306_show_all();
         passkey_req = (param != PASSKEY_STATE_SEND);
         if (param == PASSKEY_STATE_INPUT) {
             // 显示输入的配对码
-            ssd1306_clr();
             oled_draw_text_16(2, TEXT_ALIGN_CENTER, 0, (const char*)passkey);
         } 
         else if (param == PASSKEY_STATE_SEND) {
             // 清空配对码的显示
-            update_status_bar();
-            ssd1306_show_all();
+            ssd1306_clr();
         }
         status_mark_dirty();
         break;
