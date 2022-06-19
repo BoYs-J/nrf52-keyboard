0bd464863558fffe7238b22143f4d7114b6ae534
BoYs <75466116+BoYs-J@users.noreply.github.com>
2022-06-19 20:04:28 +0800

    提交

--- application/main/src/driver/ssd1306/ssd1306_oled.c
+++ application/main/src/driver/ssd1306/ssd1306_oled.c
@@ -290,6 +290,7 @@ static void ssd1306_event_handler(enum user_event event, void* arg)
                 nrf_delay_ms(10);
                 ssd1306_oled_uninit();
             }
+            status_mark_dirty();
             break;
         default:
             break;
@@ -328,7 +329,6 @@ static void ssd1306_event_handler(enum user_event event, void* arg)
             // 清空配对码的显示
             ssd1306_clr();
         }
-        ssd1306_event_handler();
         status_mark_dirty();
         break;
     case USER_EVT_BLE_STATE_CHANGE: // 蓝牙状态
