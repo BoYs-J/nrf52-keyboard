5c18cc4505cd7a5d5d2f8b1954e2dc0971fe5e79
BoYs <75466116+BoYs-J@users.noreply.github.com>
2022-06-20 13:43:05 +0800

    提交

--- application/main/src/driver/ssd1306/ssd1306_oled.c
+++ application/main/src/driver/ssd1306/ssd1306_oled.c
@@ -336,10 +336,10 @@ static void ssd1306_event_handler(enum user_event event, void* arg)
         status_mark_dirty();
         break;
 	case USER_EVT_TICK:
-        ssd1306_show_dirty_block();
+        // ssd1306_show_dirty_block();
         if (ssd1306_inited && !ssd1306_init_show) {
             ssd1306_init_show = true;
-            ssd1306_clr();
+            //ssd1306_clr();
             update_status_bar();
         }
         break;
