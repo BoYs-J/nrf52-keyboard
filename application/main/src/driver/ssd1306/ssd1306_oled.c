179e0c6083f0cbf26ac3cd2fa5182fdee1009a53
BoYs <75466116+BoYs-J@users.noreply.github.com>
2022-11-15 15:05:31 +0800

    延迟1s释放led针脚

--- application/main/src/driver/ssd1306/ssd1306_oled.c
+++ application/main/src/driver/ssd1306/ssd1306_oled.c
@@ -287,8 +287,8 @@ static void ssd1306_event_handler(enum user_event event, void* arg)
         case KBD_STATE_SLEEP: // 睡眠
             if (ssd1306_inited) {
                 ssd1306_sleep();
+                nrf_delay_ms(1000);
                 ssd1306_oled_uninit();
-                nrf_delay_ms(1000);                
             }
             break;
         default:
@@ -319,12 +319,11 @@ static void ssd1306_event_handler(enum user_event event, void* arg)
         passkey_req = (param != PASSKEY_STATE_SEND);
         if (param == PASSKEY_STATE_INPUT) {
             // 显示输入的配对码
-            oled_draw_text_16(3, TEXT_ALIGN_CENTER, 0, (const char*)passkey);
+            oled_draw_text_16(2, TEXT_ALIGN_CENTER, 0, (const char*)passkey);
         } else if (param == PASSKEY_STATE_SEND) {
             // 清空配对码的显示
-            ssd1306_oled_init(); // 初始化
-            // oled_clear_row(2);
-            // oled_clear_row(3);
+            oled_clear_row(2);
+            oled_clear_row(3);
         }
         status_mark_dirty();
         break;
